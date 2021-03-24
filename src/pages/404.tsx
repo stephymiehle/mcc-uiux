e35766ad4eca06a39c7dc6cda5fa869516c1de68
Stephy Miehle <stephy@northxsouth.co>
2021-03-24 17:23:28 -0500

    Update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,46 +1,20 @@
 import * as React from 'react';
+import { Button } from '../components/Button';
+import { ContainerLayout } from '../components/Layout';
 
-const NotFoundPage: React.FC = () => {
+const NotFoundPage: React.FC = ({ location }) => {
   return (
-    <div className="h-screen w-screen bg-grey-100 flex items-center justify-center">
-      <div className="container">
-        <div className="flex flex-col md:flex-row items-center justify-center px-5 text-grey-700">
-          <div className="flex-1">
-            <div className="text-5xl font-dark font-bold">404</div>
-            <p className="text-2xl md:text-3xl font-light leading-normal">
-              Sorry we couldn't find this page.{' '}
-            </p>
-            <p className="mb-8">
-              But dont worry, you can find plenty of other things on our
-              homepage.
-            </p>
+    <ContainerLayout location={location} title="Page Not Found">
+      <h1 className="text-6xl font-semibold text-black dark:text-white">404</h1>
+      <p className="text-2xl font-light leading-normal md:text-3xl">
+        Page Not Found
+      </p>
+      <p className="mb-8">
+        Don't worry! You can find plenty of other things on our homepage.
+      </p>
 
-            <a
-              className="px-4 inline py-3 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
-              href="/"
-            >
-              back to homepage
-            </a>
-          </div>
-          <div className="flex-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="0.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-alert-circle w-1/2"
-            >
-              <circle cx="12" cy="12" r="10" />
-              <line x1="12" y1="8" x2="12" y2="12" />
-              <line x1="12" y1="16" x2="12.01" y2="16" />
-            </svg>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Button>Back to homepage</Button>
+    </ContainerLayout>
   );
 };
 
