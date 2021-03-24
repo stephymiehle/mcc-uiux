ff2025f43ee1e4b8cd01fcab8fbe510f3a32a5de
Stephy Miehle <stephy@northxsouth.co>
2021-03-24 17:23:18 -0500

    Add ContainerLayout

--- src/components/Layout/ContainerLayout.tsx
+++ src/components/Layout/ContainerLayout.tsx
@@ -3,12 +3,12 @@ import { Footer } from '../Footer';
 import { Nav } from '../Nav';
 import { SEO } from '../SEO';
 
-interface ILayoutProps {
+interface IContainerLayoutProps {
   location: Location;
   title?: string;
 }
 
-export const Layout: React.FC<ILayoutProps> = ({
+export const ContainerLayout: React.FC<IContainerLayoutProps> = ({
   location,
   title,
   children,
@@ -17,7 +17,9 @@ export const Layout: React.FC<ILayoutProps> = ({
     <SEO pathName={location.pathname} title={title} />
     <Nav />
     <main className="flex-grow site-container text-grey-800 dark:bg-grey-900 dark:text-grey-400">
-      {children}
+      <section className="container px-4 mx-auto">
+        <div className="pb-12 mx-auto max-w-prose md:text-lg">{children}</div>
+      </section>
     </main>
     <Footer />
   </>
